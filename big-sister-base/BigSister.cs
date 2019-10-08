6a1ba3fb6c42afc10e82537ba0a03ac2be10734c
Jose Miguel Iturralde <jmiturralde@miuandes.cl>
2019-10-08 11:31:05 -0300

    Avance 4

--- big-sister-base/BigSister.cs
+++ big-sister-base/BigSister.cs
@@ -9,6 +9,28 @@ namespace big_sister_base
         public void OnAdded (object source, RequestEventArgs e)
         {
             Console.WriteLine("Product Added");
+            Product lastProduct = e.RequestCart[e.RequestCart.Count-1];
+
+            foreach(Product var in e.RequestShopList)
+            {
+                if(var.Name == lastProduct.Name)
+                {
+                    if (var.Stock > 0)
+                    {
+                        Console.WriteLine("Product confirmed by Big Sister");
+                        var.Stock -= 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product decline by Big Sister");
+                        Console.WriteLine(var.Name);
+                        Console.WriteLine(var.Stock);
+                    }
+                }
+                else
+                {
+                }
+            }
         }
     }
 }
